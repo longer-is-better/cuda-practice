06f05ae98ccf0f9dae6ff41db96617acb5f9b195
dongwei <dongwei@cambricon.com>
2023-09-04 16:25:42 +0900

    almost find you bich

--- cuda-playground/stage_memacc/lightnet/src/network.cu
+++
@@ -1,9 +0,0 @@
-#include <glog/logging.h>
-
-#include "network.cuh"
-#include "tensor.cuh"
-
-
-Network::~Network()
-{
-}
--- cuda-playground/stage_memacc/lightnet/src/operator.cuh
+++ cuda-playground/stage_memacc/lightnet/src/operator.cuh
@@ -3,7 +3,6 @@
 #include <map>
 #include <utility>
 
-#include "tensor.cuh"
 
 class Operator {
 public:
@@ -13,26 +12,17 @@ public:
 
     std::map<Operator*, bool> _prevoperators = {};  // bool: exist for topologicalSort
     std::map<Operator*, bool> _nextoperators = {};  // bool: exist
-    std::vector<Tensor*> _input_tensors;
-    std::vector<Tensor*> _output_tensors;
 
 
     Operator(){};
     Operator(bool end_of_graph): _end_of_graph(end_of_graph){
         ;
     };
-    Operator(
-        std::vector<Tensor*> input_tensors,
-        std::vector<Tensor*> output_tensors
-    );
-
     Operator(const Operator &op);
-    virtual ~Operator(){for (auto t: _output_tensors) delete t;};
 
 
 
 
-    virtual void mirror(const std::map<Tensor*, Tensor*>& tensor_map, const std::map<Operator*, Operator*>& operator_map);
     virtual int indegree();
     virtual void set_cudastream(cudaStream_t cudastream);
     virtual std::string type_str() = 0;
